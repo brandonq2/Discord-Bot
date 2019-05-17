42f4531512ba2d466b503c36e8245090dd775482
Brandon Quach <36943338+brandonq2@users.noreply.github.com>
2019-05-16 23:43:01 -0400

    Hid token

--- src/main/java/MainFiles/DiscordBot.java
+++ src/main/java/MainFiles/DiscordBot.java
@@ -9,10 +9,11 @@ import net.dv8tion.jda.core.entities.Game;
 import javax.security.auth.login.LoginException;
 
 public class DiscordBot{
+    public static yeet yeet = new yeet();
     public static JDA jda;
     public static String prefix = ">";
     public static void main(String[] args) throws LoginException{
-        jda = new JDABuilder(AccountType.BOT).setToken("NTc3Nzc4OTMxNDY2MzcxMDc0.XNqBLA.UqifrBh7uPd_8JXiuITNLc3lZBg").buildAsync();
+        jda = new JDABuilder(AccountType.BOT).setToken(yeet.secret).buildAsync();
         jda.getPresence().setGame(Game.playing("Farming Simulator 2019"));
         jda.addEventListener(new CommandHub());
     }

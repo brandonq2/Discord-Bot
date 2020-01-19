b900c89fa9cd3469d42d28bcea24320016f2a466
Brandon Quach <36943338+brandonq2@users.noreply.github.com>
2020-01-18 21:34:35 -0500

    Changed some league commands

--- src/main/java/Commands/LeagueCommands.java
+++ src/main/java/Commands/LeagueCommands.java
@@ -6,12 +6,13 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.rithms.riot.api.ApiConfig;
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.champion_mastery.dto.ChampionMastery;
 import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
 import net.rithms.riot.constant.Platform;
 
 public class LeagueCommands {
 
-    private ApiConfig config = new ApiConfig().setKey("RGAPI-b5133aed-bb79-499d-bb9b-857d37ee3649");
+    private ApiConfig config = new ApiConfig().setKey("RGAPI-ae9554a2-0230-4c5c-969f-4f9fac5d004d");
     private RiotApi api = new RiotApi(config);
 
     public void leagueInfo(GuildMessageReceivedEvent event){
@@ -21,42 +22,50 @@ public class LeagueCommands {
 
         try {
             Summoner summoner = api.getSummonerByName(Platform.NA, "MapleKombat");
+           // ChampionMastery yeet = api.getSummonerByName();
+            //yeet.getChampionId();
+            String summonerName = "";
+            for (int i = 2; i < args.length; i++){
+                summonerName += args[i];
+            }
             switch (args[1].toUpperCase()) {
                 case "BR":
-                    summoner = api.getSummonerByName(Platform.BR, args[2]);
+                    summoner = api.getSummonerByName(Platform.BR, summonerName);
                     break;
                 case "EUNE":
-                    summoner = api.getSummonerByName(Platform.EUNE, args[2]);
+                    summoner = api.getSummonerByName(Platform.EUNE, summonerName);
                     break;
                 case "EUW":
-                    summoner = api.getSummonerByName(Platform.EUW, args[2]);
+                    summoner = api.getSummonerByName(Platform.EUW, summonerName);
                     break;
                 case "JP":
-                    summoner = api.getSummonerByName(Platform.JP, args[2]);
+                    summoner = api.getSummonerByName(Platform.JP, summonerName);
                     break;
                 case "KR":
-                    summoner = api.getSummonerByName(Platform.KR, args[2]);
+                    summoner = api.getSummonerByName(Platform.KR, summonerName);
                     break;
                 case "LAN":
-                    summoner = api.getSummonerByName(Platform.LAN, args[2]);
+                    summoner = api.getSummonerByName(Platform.LAN, summonerName);
                     break;
                 case "LAS":
-                    summoner = api.getSummonerByName(Platform.LAS, args[2]);
+                    summoner = api.getSummonerByName(Platform.LAS, summonerName);
                     break;
                 case "NA":
-                    summoner = api.getSummonerByName(Platform.NA, args[2]);
+                    summoner = api.getSummonerByName(Platform.NA, summonerName);
                     break;
                 case "OCE":
-                    summoner = api.getSummonerByName(Platform.OCE, args[2]);
+                    summoner = api.getSummonerByName(Platform.OCE, summonerName);
                     break;
                 case "RU":
-                    summoner = api.getSummonerByName(Platform.RU, args[2]);
+                    summoner = api.getSummonerByName(Platform.RU, summonerName);
                     break;
                 case "TR":
-                    summoner = api.getSummonerByName(Platform.TR, args[2]);
+                    summoner = api.getSummonerByName(Platform.TR, summonerName);
                     break;
             }
+
             embed.setTitle(":joystick: Summoner Info");
+            embed.setImage()
             embed.addField("Name:", summoner.getName(), false);
             embed.addField("Level:", "" + summoner.getSummonerLevel(), false);
             channel.sendMessage(embed.build()).queue();
